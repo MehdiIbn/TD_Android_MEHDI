da216252b90c96e5f13f10ae5c076c1888a15756
IBNETTALIB <mehdi.ibnettalib@etu.unice.fr>
2016-03-07 14:14:33 +0000

    First Commit

---
+++ MyApp/app/src/main/java/com/google/samples/apps/iosched/ui/widget/TomCatTab.java
@@ -0,0 +1,23 @@
+package com.google.samples.apps.iosched.ui.widget;
+
+
+/**
+ * Created by MEHDI on 23/02/2016.
+ */
+
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+import com.example.mehdi.myapp.R;
+
+public class TomCatTab extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.apache_tomcat_tab,container,false);
+        return v;
+    }
+}
